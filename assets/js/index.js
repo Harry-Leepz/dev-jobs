fc28c69e79327f87b74f53998c3a0946163e888f
Harry Dhillon <hdhillon478@gmail.com>
2024-05-02 21:41:31 +0100

    Add html content being rendered on fetch request

--- assets/js/index.js
+++ assets/js/index.js
@@ -127,12 +127,94 @@ const clickHandler = (event) => {
 
   // get the job item id and make fetch request
   const jobItemId = jobItemEl.children[0].getAttribute("href");
-  fetch("")
+  fetch(`https://bytegrad.com/course-assets/js/2/api/jobs?/${jobItemId}`)
     .then((response) => {
-      response.json();
+      if (!response.ok) {
+        console.log("Something went wrong!");
+        return;
+      }
+      return response.json();
     })
     .then((data) => {
-      console.log(data);
+      const { jobItem } = data;
+      console.log(jobItem);
+      spinnerJobDetailsEl.classList.remove("spinner--visible");
+
+      // render html content
+      const htmlContent = `
+        <img src="${
+          jobItem.coverImgURL
+        }" alt="#" class="job-details__cover-img">
+
+        <a class="apply-btn" href="${
+          jobItem.companyURL
+        }" target="_blank">Apply <i class="fa-solid fa-square-arrow-up-right apply-btn__icon"></i></a>
+
+        <section class="job-info">
+          <div class="job-info__left">
+              <div class="job-info__badge">${jobItem.badgeLetters}</div>
+              <div class="job-info__below-badge">
+                  <time class="job-info__time">${jobItem.daysAgo}d</time>
+                  <button class="job-info__bookmark-btn">
+                      <i class="fa-solid fa-bookmark job-info__bookmark-icon"></i>
+                  </button>
+              </div>
+          </div>
+          <div class="job-info__right">
+              <h2 class="second-heading">${jobItem.title}</h2>
+              <p class="job-info__company">${jobItem.company}</p>
+              <p class="job-info__description">${jobItem.description}</p>
+              <div class="job-info__extras">
+                  <p class="job-info__extra"><i class="fa-solid fa-clock job-info__extra-icon"></i>${
+                    jobItem.duration
+                  }</p>
+                  <p class="job-info__extra"><i class="fa-solid fa-money-bill job-info__extra-icon"></i>${
+                    jobItem.salary
+                  }</p>
+                  <p class="job-info__extra"><i class="fa-solid fa-location-dot job-info__extra-icon"></i>${
+                    jobItem.location
+                  }</p>
+              </div>
+          </div>
+        </section>
+
+        <div class="job-details__other">
+            <section class="qualifications">
+                <div class="qualifications__left">
+                    <h4 class="fourth-heading">Qualifications</h4>
+                    <p class="qualifications__sub-text">Other qualifications may apply</p>
+                </div>
+                <ul class="qualifications__list">
+                    ${jobItem.qualifications
+                      .map(
+                        (item) =>
+                          `<li class="qualifications__item">${item}</li>`
+                      )
+                      .join("")}
+                </ul>
+            </section>
+
+            <section class="reviews">
+                <div class="reviews__left">
+                    <h4 class="fourth-heading">Company reviews</h4>
+                    <p class="reviews__sub-text">Recent things people are saying</p>
+                </div>
+                <ul class="reviews__list">
+                  ${jobItem.reviews
+                    .map(
+                      (review) =>
+                        `<li class="qualifications__item">${review}</li>`
+                    )
+                    .join("")}
+                </ul>
+            </section>
+        </div>
+
+        <footer class="job-details__footer">
+            <p class="job-details__footer-text">If possible, please reference that you found the job on <span class="u-bold">rmtDev</span>, we would really appreciate it!</p>
+        </footer>
+      `;
+      jobDetailsContentEl.innerHTML = htmlContent;
     })
     .catch((error) => {
       console.log(error);
