8b3fe6243b14ca0ac550b24edcec8534965935f8
Harry Dhillon <hdhillon478@gmail.com>
2024-05-09 14:45:37 +0100

    Extract rendering joblist logic into joblist file

--- assets/js/components/JobList.js
+++ assets/js/components/JobList.js
@@ -6,6 +6,42 @@ import {
 
 import renderLoading from "./Loading.js";
 
+const renderJobList = (jobListArray) => {
+  jobListArray.slice(0, 7).forEach((jobItem) => {
+    const {
+      id,
+      badgeLetters,
+      title,
+      company,
+      duration,
+      salary,
+      location,
+      daysAgo,
+    } = jobItem;
+    const jobItemsHtml = `
+        <li class="job-item">
+          <a class="job-item__link" href="${id}">
+              <div class="job-item__badge">${badgeLetters}</div>
+              <div class="job-item__middle">
+                  <h3 class="third-heading">${title}</h3>
+                  <p class="job-item__company">${company}</p>
+                  <div class="job-item__extras">
+                      <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${duration}</p>
+                      <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${salary}</p>
+                      <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${location}</p>
+                  </div>
+              </div>
+              <div class="job-item__right">
+                  <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
+                  <time class="job-item__time">${daysAgo}d</time>
+              </div>
+          </a>
+        </li>
+  `;
+    jobListSearchEl.insertAdjacentHTML("beforeend", jobItemsHtml);
+  });
+};
+
 const clickHandler = (event) => {
   event.preventDefault();
 
@@ -116,3 +152,5 @@ const clickHandler = (event) => {
 };
 
 jobListSearchEl.addEventListener("click", clickHandler);
+
+export default renderJobList;
--- assets/js/components/Search.js
+++ assets/js/components/Search.js
@@ -8,6 +8,7 @@ import {
 
 import renderError from "./Error.js";
 import renderLoading from "./Loading.js";
+import renderJobList from "./JobList.js";
 
 const submitHandler = (event) => {
   event.preventDefault();
@@ -43,40 +44,7 @@ const submitHandler = (event) => {
       // dom update
       renderLoading("search");
       numberEl.textContent = jobItems.length;
-
-      jobItems.slice(0, 7).forEach((jobItem) => {
-        const {
-          id,
-          badgeLetters,
-          title,
-          company,
-          duration,
-          salary,
-          location,
-          daysAgo,
-        } = jobItem;
-        const jobItemsHtml = `
-            <li class="job-item">
-              <a class="job-item__link" href="${id}">
-                  <div class="job-item__badge">${badgeLetters}</div>
-                  <div class="job-item__middle">
-                      <h3 class="third-heading">${title}</h3>
-                      <p class="job-item__company">${company}</p>
-                      <div class="job-item__extras">
-                          <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${duration}</p>
-                          <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${salary}</p>
-                          <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${location}</p>
-                      </div>
-                  </div>
-                  <div class="job-item__right">
-                      <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                      <time class="job-item__time">${daysAgo}d</time>
-                  </div>
-              </a>
-            </li>
-      `;
-        jobListSearchEl.insertAdjacentHTML("beforeend", jobItemsHtml);
-      });
+      renderJobList(jobItems);
     })
     .catch((error) => {
       console.log(error);
