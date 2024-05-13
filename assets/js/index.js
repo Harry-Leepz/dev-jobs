29ef85df50e75fcd69fc1f21bd91a87f8961de1b
Harry Dhillon <hdhillon478@gmail.com>
2024-05-13 21:17:27 +0100

    Update imports in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,5 @@
 import "../js/components/JobList.js";
 import "../js/components/Search.js";
+import "../js/components/Loading.js";
+import "../js/components/Error.js";
+import "../js/components/JobDetails.js";
