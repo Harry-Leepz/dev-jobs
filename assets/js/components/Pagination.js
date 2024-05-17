37c24846437ed07bfa5eddc41a1d51f3ac4defd5
Harry Dhillon <hdhillon478@gmail.com>
2024-05-17 21:53:24 +0100

    Impement pagination logic to click handler fn

--- assets/js/components/Pagination.js
+++ assets/js/components/Pagination.js
@@ -11,10 +11,19 @@ import renderJobList from "./JobList.js";
 
 const clickHandler = (event) => {
   // get the clicked element
-  const clickedButtonEl = event.target.closest("pagination__button");
+  const clickedButtonEl = event.target.closest(".pagination__button");
 
   // stop function if a non button element is clicked
   if (!clickedButtonEl) return;
+
+  // check if click button is for next or back
+  const nextPage = clickedButtonEl.className.includes("--next") ? true : false;
+
+  // update state byt incrementing or decrementing the page number
+  nextPage ? state.currentPage++ : state.currentPage--;
+
+  // render job items for the relevant page
+  renderJobList();
 };
 
 paginationEl.addEventListener("click", clickHandler);
