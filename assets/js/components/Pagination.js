ae9f514e8cd2aaf5caf3c5ae43173843407458dc
Harry Dhillon <hdhillon478@gmail.com>
2024-05-18 21:52:48 +0100

    Add render pagination button fn with working logic

--- assets/js/components/Pagination.js
+++ assets/js/components/Pagination.js
@@ -9,6 +9,30 @@ import {
 
 import renderJobList from "./JobList.js";
 
+const renderPaginationButtons = () => {
+  // display the back pagination button fi we are on page 2
+  if (state.currentPage >= 2) {
+    paginationBtnBackEl.classList.remove("pagination__button--hidden");
+  } else {
+    paginationBtnBackEl.classList.add("pagination__button--hidden");
+  }
+
+  // display the nexy pagination button if there are more job items to show on next page
+  if (state.searchJobItems.length - state.currentPage * 7 <= 0) {
+    paginationBtnNextEl.classList.add("pagination__button--hidden");
+  } else {
+    paginationBtnNextEl.classList.remove("pagination__button--hidden");
+  }
+
+  // update the page number text inside the pagination buttons
+  paginationNumberNextEl.textContent = state.currentPage + 1;
+  paginationNumberBackEl.textContent = state.currentPage - 1;
+
+  // remove focus from clicked buttons (blur)
+  paginationBtnBackEl.blur();
+  paginationBtnNextEl.blur();
+};
+
 const clickHandler = (event) => {
   // get the clicked element
   const clickedButtonEl = event.target.closest(".pagination__button");
@@ -21,6 +45,9 @@ const clickHandler = (event) => {
 
   // update state byt incrementing or decrementing the page number
   nextPage ? state.currentPage++ : state.currentPage--;
+
+  // render pagination buttons
+  renderPaginationButtons();
 
   // render job items for the relevant page
   renderJobList();
