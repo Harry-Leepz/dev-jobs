28d9b2c2d7d616c15c3a9e56e212f069e9bdd5e0
Harry Dhillon <hdhillon478@gmail.com>
2024-05-19 20:42:13 +0100

    Implement routing logic via event listener

---
+++ assets/js/components/Router.js
@@ -0,0 +1,33 @@
+import { BASE_API_URL, getData, jobDetailsContentEl } from "../common.js";
+
+import renderLoading from "./Loading.js";
+import renderJobDetails from "./JobDetails.js";
+import renderError from "./Error.js";
+
+const loadHashChangeHandler = async () => {
+  // get the job item id from url and remove the hash from the start
+  const id = location.hash.substring(1);
+
+  if (id) {
+    // remove previous job details content
+    jobDetailsContentEl.innerHTML = "";
+
+    // show loading spinner
+    renderLoading("job-details");
+
+    try {
+      const data = await getData(`${BASE_API_URL}/jobs?/${id}`);
+
+      const { jobItem } = data;
+      // DOM updates
+      renderLoading("job-details");
+      renderJobDetails(jobItem);
+    } catch (error) {
+      renderLoading("job-details");
+      renderError(error.message);
+    }
+  }
+};
+
+window.addEventListener("DOMContentLoaded", loadHashChangeHandler);
+window.addEventListener("hashchange", loadHashChangeHandler);
