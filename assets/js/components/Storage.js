c5ed9afa7dbdf6029cf4b3aee064db9527d1f112
Harry Dhillon <hdhillon478@gmail.com>
2024-05-21 16:38:56 +0100

    Add logic to parse localstorage data and update state

--- assets/js/components/Storage.js
+++ assets/js/components/Storage.js
@@ -1,1 +1,6 @@
 import { state } from "../common.js";
+
+const storedBookmarkItems = localStorage.getItem("bookmarkJobItems");
+if (storedBookmarkItems) {
+  state.bookmarksJobItems = JSON.parse(storedBookmarkItems);
+}
